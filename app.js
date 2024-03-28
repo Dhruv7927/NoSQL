cf510b4040b28443b3ec3f12db85b7a598698ed4
Dhruv7927 <dhruv2k19.3000@gmail.com>
2024-03-28 19:04:03 +0530

    Fixed the .env bugs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 8000;
 const path = require('path');
 
 const methodOverride = require('method-override');
@@ -25,8 +26,8 @@ app.use(methodOverride('_method'));
 
 app.use(express.urlencoded({extended : true}));
 
-app.listen(8000 , ()=>{
-    console.log('APP IS LISTENING TO PORT 8000');
+app.listen(port, ()=>{
+    console.log(`Server is running on ${port}`);
 });
 
 app.get('/product' , async (req , res)=>{
